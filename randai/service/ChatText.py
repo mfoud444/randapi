4888bcdf739020cd3be253723eb9a926b1610ca5
Mohammed Foud <mfoud444@gmail.com>
2024-01-24 20:52:48 +0300

    app1

--- randai/service/ChatText.py
+++ randai/service/ChatText.py
@@ -73,7 +73,7 @@ class ChatText:
         if self.model == 'llama2-70b':
             params["provider"] = 'Llama2'
         if self.model == 'gpt-4':
-            params["provider"] = 'Liaobots'
+            # params["provider"] = 'Liaobots'
             # proxy = FreeProxy().get()
             # print("proxy", proxy)
             # params["proxy"] = proxy
